59cf7c45799d9256f5b6c5489e343487fc5a29cb
Hanane Challal <hananchallal07@gmail.com>
2026-01-03 14:56:14 +0100

    my-last-update

--- script.js
+++ script.js
@@ -673,6 +673,25 @@ function updateAdminVisibility() {
   }
 }
 
+/* Script Back to the top*/ 
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add("show");
+  } else {
+    scrollTopBtn.classList.remove("show");
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+
 
 
 
